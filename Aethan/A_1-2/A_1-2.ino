5e0d555b1565bc540c1fa721e7b46b1bcdd8a968
Aethan Beaudry <beaudry.aethan@gmail.com>
2019-11-02 10:34:40 -0700

    Assignment 1-2 complete, clean code

--- Aethan/A_1-2/A_1-2.ino
+++ Aethan/A_1-2/A_1-2.ino
@@ -1,31 +1,42 @@
 // CODE BASED ON:  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
 
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int ANALOG_POT_IN = 0;  // Analog input pin that the potentiometer is attached to
+const int RED_LED = 7;
+const int BUZZER = 6;
+const int BLUE_LED = 5;
 
 int sensorValue = 0;        // value read from the pot
-int outputValue = 0;        // value in MS
+int FlashRate = 0;        // value in milliseconds
 
-void setup() {
+void setup()
+{
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
+
+  pinMode(RED_LED, OUTPUT);  
+  pinMode(BUZZER, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // read the analog in value:
-  sensorValue = analogRead(analogInPin);
-  // map it to the range of the analog out:
-  outputValue = map(sensorValue, 0, 1023, 0, 255);
-  // change the analog out value:
-  analogWrite(analogOutPin, outputValue);
+  sensorValue = analogRead(ANALOG_POT_IN);
 
+  FlashRate = map(sensorValue, 0, 1023, 0, 1000);
+
+  digitalWrite(RED_LED, HIGH);
+  digitalWrite(BUZZER, HIGH);
+  digitalWrite(BLUE_LED, LOW);
+  delay(FlashRate);                       
+  digitalWrite(RED_LED, LOW);    
+  digitalWrite(BUZZER, LOW);
+  digitalWrite(BLUE_LED, HIGH);
+  delay(FlashRate);                       
+ 
   // print the results to the Serial Monitor:
   Serial.print("sensor = ");
   Serial.print(sensorValue);
   Serial.print("\t output = ");
-  Serial.println(outputValue);
-
-  // wait 2 milliseconds before the next loop for the analog-to-digital
-  // converter to settle after the last reading:
-  
-  delay(2);
+  Serial.println(FlashRate);
 }
